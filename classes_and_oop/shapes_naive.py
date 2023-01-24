250b9e3ee63f9c7f8be8a9e42dd158b9dfb98e35
Tesla <95730807+tesla-dubois@users.noreply.github.com>
2023-01-24 18:45:45 -0500

    Update shapes_naive.py

--- classes_and_oop/shapes_naive.py
+++ classes_and_oop/shapes_naive.py
@@ -1,5 +1,5 @@
 
-from math import sqrt
+from math import sqrt, pi
 
 #%%
 # Second attempt
@@ -44,6 +44,8 @@ class Polygon:
        
         self._area = s/2
         self._perimeter = perim
+        self._ipq = 4*pi*((s/2)/perim**2)
+        #self._ipq = (s/2)/perim**2
 
     @property
     def area(self):
@@ -52,8 +54,50 @@ class Polygon:
     @property
     def perimeter(self):
         return self._perimeter
-           
-if __name__ == "__main__":
     
-    poly = Polygon([(0, 0), (1, 0), (1, 1), (0, 1), (0, 0)])
+    @property
+    def ipq(self):
+        return self._ipq
+           
+#%%
 
+class Polygon:
+    
+    def __init__(self, coords):
+        self.coords = coords
+        
+        @property
+        def coords(self):
+            return self._coords
+        
+        @coords.setter
+        def coords(self, coords):
+            self._coords = coords
+            x = [c[0] for c in coords]
+            y = [c[1] for c in coords]
+            s = 0
+            perim = 0
+            for i in range(len(coords) - 1):
+                s += x[i]*y[i+1] - x[i+1]*y[i]
+                perim += sqrt((x[i+1] - x[i])**2 + (y[i+1] - y[i])**2)
+           
+            self._area = s/2
+            self._perimeter = perim
+            self._ipq = 4*pi*((s/2)/perim**2)
+            #self._ipq = (s/2)/perim**2
+
+        @property
+        def area(self):
+            return self._area
+
+        @property
+        def perimeter(self):
+            return self._perimeter
+        
+        @property
+        def ipq(self):
+            return self._ipq
+            
+if __name__ == "__main__":
+                            
+    poly = Polygon([(0, 0), (1, 0), (1, 1), (0, 1), (0, 0)])
